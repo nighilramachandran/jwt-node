a83633ec5c428d13a6bf0775d5ebc72c643ce156
nighilramachandran <nighilramm@gmail.com>
2024-12-06 11:12:44 +0400

    new genre route

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const express = require("express");
 const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
+const genreRoute = require("./routes/genre");
 const config = require("config");
 
 const app = express();
@@ -13,6 +14,7 @@ if (!config.get("jwtPrivateKey")) {
   // 1: anything other that 0 failure
   process.exit(1);
 }
+
 mongoose
   .connect("mongodb://localhost:27017/vidly")
   .then(() => console.log("Connected to db.."))
@@ -20,4 +22,5 @@ mongoose
 
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
+app.use("/api/course", genreRoute);
 app.listen(3000, () => console.log("listing on port 3000..."));
---
+++ routes/genre.js
@@ -0,0 +1,47 @@
+const Joi = require("joi");
+const express = require("express");
+const mongoose = require("mongoose");
+
+const router = express.Router();
+
+const Genre = mongoose.model(
+  "genre",
+  new mongoose.Schema({
+    item: String,
+    price: Number,
+  })
+);
+
+router.get("/", async (req, res) => {
+  try {
+    const result = await Genre.find();
+    res.status(200).send(result);
+  } catch (err) {
+    console.log(err);
+  }
+});
+router.get("/:id", (req, res) => {
+  // let result = courses.find((c) => c.id === parseInt(req.params.id));
+  // if (!result) res.status(404).send("NO data found");
+  // res.status(200).send(result);
+});
+
+router.post("/", async (req, res) => {
+  const schema = {
+    item: Joi.string().min(3).required(),
+    price: Joi.number().required(),
+  };
+
+  let result = Joi.validate(req.body, schema);
+
+  if (result?.error) {
+    res.status(400).send(result.error.details[0].message);
+    return;
+  }
+
+  let course = new Genre({ item: req.body.item, price: req.body.price });
+  course = await course.save();
+  res.status(200).send(course);
+});
+
+module.exports = router;
