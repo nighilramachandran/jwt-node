e76138e7f29416f11a7ef1d13972169275ee6cdb
nighilramachandran <nighilramm@gmail.com>
2024-12-06 16:18:38 +0400

    central expectional error handleing module

--- routes/auth.js
+++ routes/auth.js
@@ -3,25 +3,29 @@ const express = require("express");
 const bcrypt = require("bcrypt");
 const { Users } = require("../models/user");
 const _ = require("lodash");
+const asyncMiddleware = require("../middleware/async");
 
 const router = express.Router();
 
-router.post("/", async (req, res) => {
-  const { error } = validateAuth(req.body);
+router.post(
+  "/",
+  asyncMiddleware(async (req, res) => {
+    const { error } = validateAuth(req.body);
 
-  if (error) {
-    return res.status(400).send(error.details[0].message);
-  }
-  let auth = await Users.findOne({ email: req.body.email });
-  if (!auth) return res.status(400).send("Invalid username or password");
+    if (error) {
+      return res.status(400).send(error.details[0].message);
+    }
+    let auth = await Users.findOne({ email: req.body.email });
+    if (!auth) return res.status(400).send("Invalid username or password");
 
-  let valid = await bcrypt.compare(req.body.password, auth.password);
+    let valid = await bcrypt.compare(req.body.password, auth.password);
 
-  if (!valid) return res.status(400).send("Invalid username or password");
+    if (!valid) return res.status(400).send("Invalid username or password");
 
-  const token = auth.getAuthenticationToken();
-  return res.status(200).send(token);
-});
+    const token = auth.getAuthenticationToken();
+    return res.status(200).send(token);
+  })
+);
 
 const validateAuth = (req) => {
   const schema = {
